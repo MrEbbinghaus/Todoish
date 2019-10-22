680719d4146eff82d22e0ddf82c6f15367ab4355
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2019-10-22 09:46:17 +0200

    Fix require of removed ns

--- src/main/todoish/server_components/pathom.clj
+++ src/main/todoish/server_components/pathom.clj
@@ -7,7 +7,6 @@
     [com.wsscode.common.async-clj :refer [let-chan]]
     [clojure.core.async :as async]
     [todoish.model.account :as acct]
-    [todoish.model.session :as session]
     [todoish.server-components.config :refer [config]]
     [todoish.model.mock-database :as db]))
 
@@ -19,7 +18,7 @@
      (update ::pc/index-resolvers #(into [] (map (fn [[k v]] [k (dissoc v ::pc/resolve)])) %))
      (update ::pc/index-mutations #(into [] (map (fn [[k v]] [k (dissoc v ::pc/mutate)])) %)))})
 
-(def all-resolvers [acct/resolvers session/resolvers index-explorer])
+(def all-resolvers [acct/resolvers index-explorer])
 
 (defn preprocess-parser-plugin
   "Helper to create a plugin that can view/modify the env/tx of a top-level request.
