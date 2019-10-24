d805f8ae34453c701d137c6b65d8178d9b9c9e81
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2019-10-24 14:04:04 +0200

    Simplify code base for presentation

--- src/main/todoish/client.cljs
+++ src/main/todoish/client.cljs
@@ -1,25 +1,21 @@
 (ns todoish.client
   (:require
-    [todoish.application :refer [SPA]]
     [com.fulcrologic.fulcro.application :as app]
-    [todoish.ui.root :as root]
     [com.fulcrologic.fulcro.networking.http-remote :as net]
     [com.fulcrologic.fulcro.data-fetch :as df]
-    [com.fulcrologic.fulcro.ui-state-machines :as uism]
-    [com.fulcrologic.fulcro.components :as comp]
-    [com.fulcrologic.fulcro-css.css-injection :as cssi]
     [taoensso.timbre :as log]
-    [com.fulcrologic.fulcro.algorithms.denormalize :as fdn]
-    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
-    [com.fulcrologic.fulcro.inspect.inspect-client :as inspect]))
+    [todoish.ui.root :as root]
+    [todoish.models.todo :as todo]))
+
+(defonce SPA (app/fulcro-app
+               {:client-did-mount (fn [app] (df/load! app :all-todos todo/Todo))
+                :remotes {:remote (net/fulcro-http-remote
+                                    {:url                "/api"})}}))
 
 (defn ^:export refresh []
   (log/info "Hot code Remount")
-  (cssi/upsert-css "componentcss" {:component root/Root})
   (app/mount! SPA root/Root "todoish"))
 
 (defn ^:export init []
   (log/info "Application starting.")
-  (cssi/upsert-css "componentcss" {:component root/Root})
-  ;(inspect/app-started! SPA))
-  (app/mount! SPA root/Root "todoish" #_{:initialize-state? false}))
+  (app/mount! SPA root/Root "todoish"))
--- src/main/todoish/server_main.clj
+++
@@ -1,9 +0,0 @@
-(ns todoish.server-main
-  (:require
-    [mount.core :as mount]
-    todoish.server-components.http-server)
-  (:gen-class))
-
-;; This is a separate file for the uberjar only. We control the server in dev mode from src/dev/user.clj
-(defn -main [& args]
-  (mount/start-with-args {:config "config/prod.edn"}))
